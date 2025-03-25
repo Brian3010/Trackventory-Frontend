a7e38cfb402dd399d14190b22ae6b743c119424f
Brian <48544476+Brian3010@users.noreply.github.com>
2025-03-25 22:00:16 +1100

    setting up

---
+++ app/dashboard/inventory/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Inventory = () => {
+  return (
+    <div>
+      <div>Inventory Page</div>
+      {/* list all the categories  */}
+    </div>
+  );
+};
+
+export default Inventory;
