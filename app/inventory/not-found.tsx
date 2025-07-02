bda2001ee590359cbe858734caf515f1ba7b2d86
Brian <48544476+Brian3010@users.noreply.github.com>
2025-07-03 01:30:37 +1000

    added not found pages

--- app/inventory/not-found.tsx
+++ app/inventory/not-found.tsx
@@ -5,8 +5,12 @@ export default function NotFound() {
     <section className="bg-white h-screen flex items-center">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-3xl tracking-tight font-extrabold lg:text-5xl text-primary-600">404</h1>
-          <p className="mb-4 text-xl tracking-tight font-bold md:text-2xl">Page Not Found</p>
+          <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-white">
+            404
+          </h1>
+          <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl dark:text-white">
+            Page Not Found
+          </p>
           <a
             href="/"
             className="inline-flex text-white bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4"
