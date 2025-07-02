50b658089fb872df843bc6e47625c6258f4943a1
Brian <48544476+Brian3010@users.noreply.github.com>
2025-07-03 01:31:15 +1000

    wrap contents in a main container

--- app/components/MainContainer.tsx
+++ app/components/MainContainer.tsx
@@ -2,7 +2,9 @@
 
 import { FormatRoute } from '@/app/utils/Helpers';
 import { usePathname } from 'next/navigation';
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useEffect } from 'react';
+import { useGlobalStore } from '../stores/useGlobalStore';
+import QueryProviders from './QueryProviders';
 
 type MainContainerProps = {
   children: ReactNode;
@@ -11,11 +13,16 @@ type MainContainerProps = {
 
 export default function MainContainer({ children, className }: MainContainerProps) {
   const pathname = usePathname();
+  const { setOpenSideBar } = useGlobalStore();
+
+  useEffect(() => {
+    if (pathname) setOpenSideBar(false);
+  }, [pathname]);
 
   return (
     <div className={className}>
       <h1 className="font-semibold text-lg mb-2.5">{FormatRoute(pathname)}</h1>
-      {children}
+      <QueryProviders>{children}</QueryProviders>
     </div>
   );
 }
